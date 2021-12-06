51d649f6d6a9199213e0380f6ac3660fdd05fa07
SamikBhatia <94806847+SamikBhatia@users.noreply.github.com>
2021-12-05 22:41:43 -0500

    Net income added

--- ausCode.py
+++ ausCode.py
@@ -1,25 +1,30 @@
-def australia(a, b, owed, perc):
+def australia(a, b, owed, perc, net):
    austx = [0, 0.19, 0.325, 0.37, 0.45]
    ausbrk = [0, 18200, 45000, 120000, 180000]
-   c = a.get()
    inc = int(b.get())
    
    if inc in range(ausbrk[0], ausbrk[1]):
       tax = round(austx[0]*inc, 2)
       p = round((austx[0]*inc / inc) * 100, 1)
+      n = inc
    elif inc in range(ausbrk[1], ausbrk[2]):
       tax = round(austx[1]*(inc - ausbrk[1]), 2)
       p = round((austx[1]*(inc - ausbrk[1])) / inc * 100, 1)
+      n = inc - (austx[1]*(inc - ausbrk[1]))
    elif inc in range(ausbrk[2], ausbrk[3]):
       tax = round(5092 + austx[2]*(inc - ausbrk[2]), 2)
       p = round((5092 + austx[2]*(inc - ausbrk[2])) / inc * 100, 1)
+      n = inc - (5092 + austx[2]*(inc - ausbrk[2]))
    elif inc in range(ausbrk[3], ausbrk[4]):
       tax = round(29467 + austx[3]*(inc - ausbrk[3]), 2)
       p = round((29467 + austx[3]*(inc - ausbrk[3])) / inc * 100, 1)
+      n = inc - (29467 + austx[3]*(inc - ausbrk[3]))
    elif inc > ausbrk[4]:
       tax = round(51667 + austx[4]*(inc - ausbrk[4]), 2)
       p = round((51667 + austx[4]*(inc - ausbrk[4])) / inc * 100, 1)
+      n = inc - (51667 + austx[4]*(inc - ausbrk[4]))
    else:
       owed.config(text = "Invalid input"), perc.config(text = "null")
    owed.config(text = "A$" + str(tax))
+   net.config(text = "A$" + str(n))
    perc.config(text = str(p) + "%")
--- canCode.py
+++ canCode.py
@@ -1,29 +1,35 @@
-def canada(a, b, owed, perc):
+def canada(a, b, owed, perc, net):
     cantx = [0.15, 0.205, 0.26, 0.29, 0.33]
     canbrk = [0, 49020, 98040, 151978, 216511]
     sd = 13229
-    c = a.get()
     inc = int(b.get())
    
     if inc in range(canbrk[0], sd):
         tax = round(cantx[0]*inc, 2)
         p = round((cantx[0]*inc) / inc * 100, 1)
+        n = inc - (cantx[0]*inc)
     elif inc - sd in range(1, canbrk[1]):
         tax = round(cantx[0]*(inc - sd), 2)
         p = round((cantx[0]*(inc - sd)) / inc * 100, 1)
+        n = inc - (cantx[0]*(inc - sd))
     elif inc - sd in range(canbrk[1], canbrk[2]):
         tax = round(7353 + cantx[1]*(inc-canbrk[1] - sd), 2)
         p = round((7353 + cantx[1]*(inc-canbrk[1] - sd)) / inc * 100, 1)
+        n = inc - (7353 + cantx[1]*(inc-canbrk[1] - sd))
     elif inc - sd in range(canbrk[2], canbrk[3]):
         tax = round(17402 + cantx[2]*(inc-canbrk[2] - sd), 2)
         p = round((17402 + cantx[2]*(inc-canbrk[2] - sd))/ inc * 100, 1)
+        n = inc - (17402 + cantx[2]*(inc-canbrk[2] - sd))
     elif inc in range(canbrk[3], canbrk[4]):
         tax = round(31426 + cantx[3]*(inc-canbrk[3] - sd), 2)
         p = round((31426 + cantx[3]*(inc-canbrk[3] - sd)) / inc * 100, 1)
+        n = inc - (31426 + cantx[3]*(inc-canbrk[3] - sd))
     elif inc - 12298 >= canbrk[4]:
         tax = round(50140 + cantx[4]*(inc-canbrk[4] - 12298), 2)
         p = round((50140 + cantx[4]*(inc-canbrk[4] - 12298)) / inc * 100, 1)
+        n = inc - (50140 + cantx[4]*(inc-canbrk[4] - 12298))
     else:
         owed.config(text = "Invalid input"), perc.config(text = "null")
     owed.config(text = "C$" + str(tax))
+    net.config(text = "C$" + str(n))
     perc.config(text = str(p) + "%")
