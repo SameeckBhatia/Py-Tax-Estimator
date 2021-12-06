dfbfd089c3f9cf8943d74fe1f740e5bf8e04335b
SamikBhatia <94806847+SamikBhatia@users.noreply.github.com>
2021-12-05 22:42:25 -0500

    Updated UI

--- mainCalc.py
+++ mainCalc.py
@@ -14,36 +14,38 @@ win.geometry("550x350")
 #Defining tax function
 def tax():
     if a.get().lower() == "australia":
-        australia(a, b, owed, perc)
+        australia(a, b, owed, perc, net)
     elif a.get().lower() == "canada":
-        canada(a, b, owed, perc)
+        canada(a, b, owed, perc, net)
     elif a.get().lower() == "uk":
-        uk(a, b, owed, perc)
+        uk(a, b, owed, perc, net)
     elif a.get().lower() == "us" or a.get().lower() == "usa":
-        states(a, b, owed, perc)
+        states(a, b, owed, perc, net)
     else:
-        owed.config(text = "Invalid input")
-        perc.config(text = "null%")
+        owed.config(text = "invalid", font = "Courier 11 bold")
+        net.config(text = "invalid", font = "Courier 11 bold")
+        perc.config(text = "null", font = "Courier 11 bold")
 
 #Collecting input to calculate based on country
-Label(win, text="Enter country: ", font = "Bahnschrift 11").place(x = "50", y = "40")
-a = Entry(win, width=20)
+Label(win, text = "Enter country: ", font = "Bahnschrift 11").place(x = "50", y = "40")
+a = Entry(win, width = 20)
 a.place(x = "50", y = "80")
 
 #Collecting income to calculate tax owed
-Label(win, text="Enter income: ", font = "Bahnschrift 11").place(x = "50", y = "140")
-b = Entry(win, width=20)
+Label(win, text = "Enter income: ", font = "Bahnschrift 11").place(x = "50", y = "140")
+b = Entry(win, width = 20)
 b.place(x = "50", y = "180")
 
-#Creating buttons
-Button(win, text="Calculate", font = "Bahnschrift 11", height = "1", width = "15", command = tax).place(x = "50", y = "255")
+Button(win, text="Calculate", font = "Bahnschrift 11", height = "1", width = "15", comman = tax).place(x = "50", y = "255")
 Button(win, text="End program", font = "Bahnschrift 11", height = "1", width = "15", command = win.quit).place(x = "300", y = "255")
 
-#Placeholder labbels to fill in tax owed and percentage
-owed = Label(win, text="<tax owed>", font = "Bahnschrift 11")
-owed.place(x = "330", y = "65")
+owed = Label(win, text = "<tax owed>", font = "Bahnschrift 11")
+owed.place(x = "330", y = "50")
 
-perc = Label(win, text="<tax percent>", font = "Bahnschrift 11")
-perc.place(x = "325", y = "165")
+net = Label(win, text = "<net income>", font = "Bahnschrift 11")
+net.place(x = "330", y = "115")
+
+perc = Label(win, text = "<tax percent>", font = "Bahnschrift 11")
+perc.place(x = "330", y = "180")
 
 win.mainloop()
