0d69154a4ac5887cd9d223b7ae403e1a99a32ccb
SamikBhatia <94806847+SamikBhatia@users.noreply.github.com>
2021-12-08 16:35:30 -0500

    Added rounding

--- amCode.py
+++ amCode.py
@@ -7,35 +7,35 @@ def states(a, b, owed, perc, net):
     if inc in range(usbrk[0], sd):
         tax = round(ustx[0]*inc, 2)
         p = round((ustx[0]*inc) / inc * 100, 1)
-        n = inc
+        n = round(inc, 2)
     elif inc - sd in range(1, usbrk[1]):
         tax = round(ustx[1]*(inc - sd), 2)
         p = round((ustx[1]*(inc - sd)) / inc * 100, 1)
-        n = inc - ustx[1]*(inc - sd)
+        n = round(inc - ustx[1]*(inc - sd), 2)
     elif inc - sd in range(usbrk[1], usbrk[2]):
         tax = round(995 + ustx[2]*(inc-usbrk[1] - sd), 2)
         p = round((995 + ustx[2]*(inc-usbrk[1] - sd)) / inc * 100, 1)
-        n = inc - (995 + ustx[2]*(inc-usbrk[1] - sd))
+        n = round(inc - (995 + ustx[2]*(inc-usbrk[1] - sd)), 2)
     elif inc - sd in range(usbrk[2], usbrk[3]):
         tax = round(4664 + ustx[3]*(inc-usbrk[2] - sd), 2)
         p = round((4664 + ustx[3]*(inc-usbrk[2] - sd)) / inc * 100, 1)
-        n = inc - (4664 + ustx[3]*(inc-usbrk[2] - sd))
+        n = round(inc - (4664 + ustx[3]*(inc-usbrk[2] - sd)), 2)
     elif inc - sd in range(usbrk[3], usbrk[4]):
         tax = round(14751 + ustx[4]*(inc-usbrk[3] - sd), 2)
         p = round((14751 + ustx[4]*(inc-usbrk[3] - sd)) / inc * 100, 1)
-        n = inc - (14751 + ustx[4]*(inc-usbrk[3] - sd))
+        n = round(inc - (14751 + ustx[4]*(inc-usbrk[3] - sd)), 2)
     elif inc - sd in range(usbrk[4], usbrk[5]):
         tax = round(33603 + ustx[5]*(inc-usbrk[4] - sd), 2)
         p = round((33603 + ustx[5]*(inc-usbrk[4] - sd)) / inc * 100, 1)
-        n = inc - (33603 + ustx[5]*(inc-usbrk[4] - sd))
+        n = round(inc - (33603 + ustx[5]*(inc-usbrk[4] - sd)), 2)
     elif inc - sd in range(usbrk[5], usbrk[6]):
         tax = round(47843 + ustx[6]*(inc-usbrk[5] - sd), 2)
         p = round((47843 + ustx[6]*(inc-usbrk[5] - sd)) / inc * 100, 1)
-        n = inc - (47843 + ustx[6]*(inc-usbrk[5] - sd))
+        n = round(inc - (47843 + ustx[6]*(inc-usbrk[5] - sd)), 2)
     elif inc - sd >= usbrk[6]:
         tax = round(157804 + ustx[7]*(inc-usbrk[6] - sd), 2)
         p = round((157804 + ustx[7]*(inc-usbrk[6] - sd)) / inc * 100, 1)
-        n = inc - (157804 + ustx[7]*(inc-usbrk[6] - sd))
+        n = round(inc - (157804 + ustx[7]*(inc-usbrk[6] - sd)), 2)
     else:
         owed.config(text = "Invalid input"), perc.config(text = "null")
     owed.config(text = "$" + str(tax))
--- canCode.py
+++ canCode.py
@@ -7,27 +7,27 @@ def canada(a, b, owed, perc, net):
     if inc in range(canbrk[0], sd):
         tax = round(cantx[0]*inc, 2)
         p = round((cantx[0]*inc) / inc * 100, 1)
-        n = inc - (cantx[0]*inc)
+        n = round(inc - (cantx[0]*inc), 2)
     elif inc - sd in range(1, canbrk[1]):
         tax = round(cantx[0]*(inc - sd), 2)
         p = round((cantx[0]*(inc - sd)) / inc * 100, 1)
-        n = inc - (cantx[0]*(inc - sd))
+        n = round(inc - (cantx[0]*(inc - sd)), 2)
     elif inc - sd in range(canbrk[1], canbrk[2]):
         tax = round(7353 + cantx[1]*(inc-canbrk[1] - sd), 2)
         p = round((7353 + cantx[1]*(inc-canbrk[1] - sd)) / inc * 100, 1)
-        n = inc - (7353 + cantx[1]*(inc-canbrk[1] - sd))
+        n = round(inc - (7353 + cantx[1]*(inc-canbrk[1] - sd)), 2)
     elif inc - sd in range(canbrk[2], canbrk[3]):
         tax = round(17402 + cantx[2]*(inc-canbrk[2] - sd), 2)
         p = round((17402 + cantx[2]*(inc-canbrk[2] - sd))/ inc * 100, 1)
-        n = inc - (17402 + cantx[2]*(inc-canbrk[2] - sd))
+        n = round(inc - (17402 + cantx[2]*(inc-canbrk[2] - sd)), 2)
     elif inc in range(canbrk[3], canbrk[4]):
         tax = round(31426 + cantx[3]*(inc-canbrk[3] - sd), 2)
         p = round((31426 + cantx[3]*(inc-canbrk[3] - sd)) / inc * 100, 1)
-        n = inc - (31426 + cantx[3]*(inc-canbrk[3] - sd))
+        n = round(inc - (31426 + cantx[3]*(inc-canbrk[3] - sd)), 2)
     elif inc - 12298 >= canbrk[4]:
         tax = round(50140 + cantx[4]*(inc-canbrk[4] - 12298), 2)
         p = round((50140 + cantx[4]*(inc-canbrk[4] - 12298)) / inc * 100, 1)
-        n = inc - (50140 + cantx[4]*(inc-canbrk[4] - 12298))
+        n = round(inc - (50140 + cantx[4]*(inc-canbrk[4] - 12298)), 2)
     else:
         owed.config(text = "Invalid input"), perc.config(text = "null")
     owed.config(text = "C$" + str(tax))
