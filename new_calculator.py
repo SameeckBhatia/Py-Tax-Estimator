e4f39530ab57a5586b026ac02a4efd3e4839750e
SameeckBhatia <canadianmg@outlook.com>
2024-03-20 21:10:00 -0400

    Creating a class for federal income tax

--- new_calculator.py
+++ new_calculator.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import numpy as np
 from typing import Union
+
+
+class Federal:
+
+    def __init__(self, country: str, income: float) -> None:
+        self.country = country
+        self.income = income
