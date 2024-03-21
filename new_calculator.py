deb164a3ea17ff7c90ad2f958d96c370417f9293
SameeckBhatia <canadianmg@outlook.com>
2024-03-21 09:49:48 -0400

    Defining a method for income deductions

--- new_calculator.py
+++ new_calculator.py
@@ -8,3 +8,16 @@ class Federal:
     def __init__(self, country: str, income: float) -> None:
         self.country = country
         self.income = income
+
+    def standard_deduction(self) -> float:
+        if self.country == "Canada":
+            if self.income <= 173205:
+                return 15705
+            elif self.income >= 246752:
+                return 14156
+            else:
+                return -0.02105 * (self.income - 173205) + 15705
+        elif self.country == "United States":
+            return 13850
+        else:
+            return 0
