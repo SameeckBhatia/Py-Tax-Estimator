5df147df66ea3765fca1d8f40de526c598deaa4c
SameeckBhatia <canadianmg@outlook.com>
2024-04-04 01:26:30 -0400

    Reformatting the window

--- interface.py
+++ interface.py
@@ -1,11 +1,13 @@
 import tkinter as tk
+from calculator import *
 from ctypes import windll
 
 windll.shcore.SetProcessDpiAwareness(2)
 
 win = tk.Tk()
-win.geometry("500x400")
-
+win.geometry("400x600")
+win.config(background="#2D3339")
+win.resizable(width=False, height=False)
 win.title("Tax Estimator")
 
 win.mainloop()
