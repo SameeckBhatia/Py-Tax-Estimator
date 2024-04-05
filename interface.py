cff44444cd491a167aec28fd4614a3c8850a6c86
SameeckBhatia <canadianmg@outlook.com>
2024-04-05 18:53:05 -0400

    Creating labels and dropdowns

--- interface.py
+++ interface.py
@@ -10,4 +10,25 @@ win.config(background="#2D3339")
 win.resizable(width=False, height=False)
 win.title("Tax Estimator")
 
+# Labels
+label1 = tk.Label(win, text="Select country", font=("Segoe UI", 14),
+                  background="#2D3339", foreground="#FBF4E9")
+label1.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
+
+label2 = tk.Label(win, text="Select state/province", font=("Segoe UI", 14),
+                  background="#2D3339", foreground="#FBF4E9")
+label2.place(relx=0.5, rely=0.35, anchor=tk.CENTER)
+
+# Dropdowns
+options = ["Canada", "United States"]
+choice1 = tk.StringVar(win)
+choice1.set("(country)")
+dropdown1 = tk.OptionMenu(win, choice1, *options)
+dropdown1.place(relx=0.5, rely=0.2, relwidth=0.5, anchor=tk.CENTER)
+
+choice2 = tk.StringVar(win)
+choice2.set("(state/province)")
+dropdown2 = tk.OptionMenu(win, choice2, "")
+dropdown2.place(relx=0.5, rely=0.45, relwidth=0.5, anchor=tk.CENTER)
+
 win.mainloop()
